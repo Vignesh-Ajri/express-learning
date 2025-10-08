e76cceea92be4aba43d55d26e3c476dea9ec53da
VIGNESH <ssvignesh2003@gmail.com>
2025-10-08 17:53:30 +0530

    first server

---
+++ phase-1-foundations/1-getting-started/express-basics/1-first-server/server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+
+//simple route
+app.get("/",(req, res) =>{
+    res.send("Hello World!!")
+});
+
+//server
+app.listen(3000,()=>{
+    console.log("Server is running on http://localhost:3000")
+})
