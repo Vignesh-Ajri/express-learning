cca0f0c2886c970383d63ed38834d6929b598398
VIGNESH <ssvignesh2003@gmail.com>
2025-10-08 18:20:13 +0530

    Built in middleware update

---
+++ phase-1-foundations/1-getting-started/express-basics/2-middleware/built-in-middleware.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+// Built-in middleware to serve static files (e.g., HTML, CSS, images)
+app.use(express.static(path.join(__dirname, "public")));
+
+// Built-in middleware to parse JSON body
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Built-in middleware example");
+});
+
+app.post("/data", (req, res) => {
+  res.json({ message: "Data received", body: req.body });
+});
+
+app.listen(3000, () => {
+  console.log("Built-in middleware server running at http://localhost:3000");
+});
