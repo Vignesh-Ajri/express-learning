7094462862005930c39c37497c203a485340702c
VIGNESH <ssvignesh2003@gmail.com>
2025-10-13 19:32:54 +0530

    User and Post Controller file update

---
+++ phase-1-foundations/1-getting-started/express-basics/5-database/mongodb-mongoose/controllers/postController.js
@@ -0,0 +1,23 @@
+const Post = require('../models/Post');
+
+// GET all posts
+const getPosts = async (req, res) => {
+  try {
+    const posts = await Post.find().populate('author');
+    res.json(posts);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// CREATE a new post
+const createPost = async (req, res) => {
+  try {
+    const newPost = await Post.create(req.body);
+    res.status(201).json(newPost);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+module.exports = { getPosts, createPost };
---
+++ phase-1-foundations/1-getting-started/express-basics/5-database/mongodb-mongoose/controllers/userController.js
@@ -0,0 +1,23 @@
+const User = require('../models/User');
+
+// GET all users
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// CREATE a new user
+const createUser = async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+module.exports = { getUsers, createUser };
