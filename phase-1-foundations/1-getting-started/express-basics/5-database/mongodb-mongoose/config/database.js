72bd5cd5c43f052857a7e87631844142309830f5
VIGNESH <ssvignesh2003@gmail.com>
2025-10-13 19:43:20 +0530

    MongoDb database config file update

--- phase-1-foundations/1-getting-started/express-basics/5-database/mongodb-mongoose/config/database.js
+++ phase-1-foundations/1-getting-started/express-basics/5-database/mongodb-mongoose/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/defaultdb");
     console.log('MongoDB Connected Successfully');
   } catch (error) {
     console.error('Database connection failed:', error.message);
