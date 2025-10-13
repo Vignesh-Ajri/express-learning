7553623f5c7f29e4e1d57d1d75e93da57b2ae56a
VIGNESH <ssvignesh2003@gmail.com>
2025-10-13 19:23:29 +0530

    MongoDb Server file initial server setup

---
+++ phase-1-foundations/1-getting-started/express-basics/5-database/mongodb-mongoose/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
