c69488f929ec591777ea0c8c40fdad8692a85ba5
VIGNESH <ssvignesh2003@gmail.com>
2025-10-13 19:35:38 +0530

    MongoDb Server fileupdate

--- phase-1-foundations/1-getting-started/express-basics/5-database/mongodb-mongoose/server.js
+++ phase-1-foundations/1-getting-started/express-basics/5-database/mongodb-mongoose/server.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 
+const userRoutes = require('./routes/users')
+const postRoutes = require('./routes/posts')
 const connectDB = require('./config/database');
 
 dotenv.config();
@@ -11,6 +13,9 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
