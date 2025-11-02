2aad6cc4c5526c19858a64e76385771ecf8e2e33
VIGNESH <ssvignesh2003@gmail.com>
2025-11-02 13:56:05 +0530

    Server update and database configuration

--- phase-1-foundations/3-advanced-api-features/server.js
+++ phase-1-foundations/3-advanced-api-features/server.js
@@ -1,6 +1,13 @@
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+const connectDB = require('./config/database');
+
+dotenv.config();
+connectDB();
+
+const app = express()
 app.listen(3000,()=>{
     console.log("server is running on http://localhost:3000")
 })
