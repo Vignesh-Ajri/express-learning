f501f233ef43a6c7dcde0a38fa6c1f0871fa52ac
VIGNESH <ssvignesh2003@gmail.com>
2025-11-03 23:39:58 +0530

    Filtering using query parameter

--- phase-1-foundations/3-advanced-api-features/api-features/filtering.js
+++ phase-1-foundations/3-advanced-api-features/api-features/filtering.js
@@ -1,16 +1,16 @@
 exports.filterUsers = async (req, res, next, Model) => {
   try {
-    // 1️⃣ Make a copy of the query object
+    // 1️ Make a copy of the query object
     const queryObj = { ...req.query };
 
-    // 2️⃣ Remove fields that are not filters (e.g. sort, page, etc.)
+    // 2️ Remove fields that are not filters (e.g. sort, page, etc.)
     const excludeFields = ["sort", "limit", "page", "fields"];
     excludeFields.forEach((el) => delete queryObj[el]);
 
-    // 3️⃣ Build the query
+    // 3️ Build the query
     let query = Model.find(queryObj);
 
-    // 4️⃣ Execute
+    // 4️ Execute
     const docs = await query;
 
     res.status(200).json({
