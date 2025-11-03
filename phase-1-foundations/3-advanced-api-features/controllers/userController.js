4cb714a5a69469b068d777d5de6adffae48ef3a5
VIGNESH <ssvignesh2003@gmail.com>
2025-11-03 23:41:26 +0530

    Filtering controller

--- phase-1-foundations/3-advanced-api-features/controllers/userController.js
+++ phase-1-foundations/3-advanced-api-features/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/Users");
+const { filterUsers } = require("../api-features/filtering");
 
 // @desc    Get all users
 // @route   GET /api/v1/users
@@ -20,4 +21,11 @@ exports.getAllUsers = async (req, res) => {
       message: err.message,
     });
   }
+};
+
+// @desc    Get all users (with filtering support)
+// @route   GET /api/v1/users
+// @access  Public
+exports.getAllUsers = async (req, res) => {
+  await filterUsers(req, res, null, User);
 };
