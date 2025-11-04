d9bd0ca3b3148e7fdb4632518d43132c7883dd58
VIGNESH <ssvignesh2003@gmail.com>
2025-11-04 23:22:54 +0530

    Pagination Feature user controller update

--- phase-1-foundations/3-advanced-api-features/controllers/userController.js
+++ phase-1-foundations/3-advanced-api-features/controllers/userController.js
@@ -2,12 +2,18 @@ const User = require("../models/User");
 const { filterUsers } = require("../api-features/filtering");
 const { sortResults } = require("../api-features/sorting");
 const { limitFields } = require("../api-features/field-limiting");
+const { paginateResults } = require("../api-features/pagination");
 
+// @desc    Get all users (supports filter, sort, fields, pagination)
+// @route   GET /api/v1/users
+// @access  Public
 exports.getAllUsers = async (req, res) => {
   if (req.query.sort) {
     await sortResults(req, res, User);
   } else if (req.query.fields) {
     await limitFields(req, res, User);
+  } else if (req.query.page || req.query.limit) {
+    await paginateResults(req, res, User);
   } else {
     await filterUsers(req, res, null, User);
   }
