a5a9b904f15946041b3284c1587bd9b80e925ac3
VIGNESH <ssvignesh2003@gmail.com>
2025-11-04 22:32:34 +0530

    Field Limiting feature UserController update

--- phase-1-foundations/3-advanced-api-features/controllers/userController.js
+++ phase-1-foundations/3-advanced-api-features/controllers/userController.js
@@ -1,14 +1,13 @@
 const User = require("../models/User");
 const { filterUsers } = require("../api-features/filtering");
 const { sortResults } = require("../api-features/sorting");
+const { limitFields } = require("../api-features/field-limiting");
 
-// @desc    Get all users (filtering + sorting)
-// @route   GET /api/v1/users
-// @access  Public
 exports.getAllUsers = async (req, res) => {
-  // 🧩 Choose feature based on query
   if (req.query.sort) {
     await sortResults(req, res, User);
+  } else if (req.query.fields) {
+    await limitFields(req, res, User);
   } else {
     await filterUsers(req, res, null, User);
   }
