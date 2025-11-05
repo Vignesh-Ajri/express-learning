328ec0c32d53be243d94ee6280541169ce1f37f9
VIGNESH <ssvignesh2003@gmail.com>
2025-11-05 23:44:19 +0530

    Aggregation analytics routes update

--- phase-1-foundations/3-advanced-api-features/routes/users.js
+++ phase-1-foundations/3-advanced-api-features/routes/users.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const { getUserStats } = require("../aggregation/analytics");
 
 // Define route
 router.get("/", userController.getAllUsers);
+router.get("/stats", getUserStats);
 
 module.exports = router;
