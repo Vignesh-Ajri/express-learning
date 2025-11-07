73d457c4622f554de6bff26bc40a4939b7043b66
VIGNESH <ssvignesh2003@gmail.com>
2025-11-07 21:37:39 +0530

    File Update

---
+++ phase-1-foundations/3-advanced-api-features/versioning/api-versioning.js
@@ -0,0 +1,8 @@
+const v1UserRoutes = require("./v1/userRoutes");
+const v2UserRoutes = require("./v2/userRoutes");
+
+exports.setupVersioning = (app) => {
+  app.use("/api/v1/users", v1UserRoutes);
+  app.use("/api/v2/users", v2UserRoutes);
+  console.log("✅ API Versioning enabled (v1 & v2)");
+};
