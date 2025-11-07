fdc1ee46b3997c3bc1290627863ffc4d16e3d9a4
VIGNESH <ssvignesh2003@gmail.com>
2025-11-07 21:57:28 +0530

    User post routes

--- phase-1-foundations/3-advanced-api-features/routes/users.js
+++ phase-1-foundations/3-advanced-api-features/routes/users.js
@@ -2,9 +2,13 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 const { getUserStats } = require("../aggregation/analytics");
+const nestedPostRoutes = require("../nested-population/nested-routes");
 
 // Define route
 router.get("/", userController.getAllUsers);
 router.get("/stats", getUserStats);
+
+// Nested route: /api/v1/users/:userId/posts
+router.use("/:userId/posts", nestedPostRoutes);
 
 module.exports = router;
