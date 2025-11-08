ae6da8297f6a84758b4c57f06db964379408e80a
VIGNESH <ssvignesh2003@gmail.com>
2025-11-08 23:17:26 +0530

    Server update

--- phase-1-foundations/3-advanced-api-features/server.js
+++ phase-1-foundations/3-advanced-api-features/server.js
@@ -1,14 +1,10 @@
 const express = require('express')
 const dotenv = require('dotenv');
 const connectDB = require('./config/database');
-const { setSecurityHeaders } = require("./performance/helmet");
-const { applyRateLimit } = require("./performance/rate-limit");
-const { preventHpp } = require("./performance/hpp");
-const { enableCompression } = require("./performance/compression");
 const { logQueryTime } = require("./performance/optimization");
 const { loadEnv } = require("./security/dotenv-config");
-
 const { setupVersioning } = require("./versioning/api-versioning");
+const { applySecurityMiddleware } = require("./security/security-middleware");
 
 loadEnv();
 dotenv.config();
@@ -16,14 +12,11 @@ connectDB();
 
 const app = express();
 setupVersioning(app);
-
-// Middleware
 app.use(express.json());
 app.use(logQueryTime);
-setSecurityHeaders(app);
-applyRateLimit(app);
-preventHpp(app);
-enableCompression(app);
+
+// Apply all security + performance middlewares
+applySecurityMiddleware(app);
 
 // Routes
 const userRoutes = require("./routes/users");
@@ -37,7 +30,6 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT,()=>{
     console.log(`server is running on http://localhost:${PORT}`);
 })
