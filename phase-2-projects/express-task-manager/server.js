012cf90c126077424a46bb704e63b51a28cfc87a
VIGNESH <ssvignesh2003@gmail.com>
2025-11-09 00:00:24 +0530

    Express task manager server setup

---
+++ phase-2-projects/express-task-manager/server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan')
+const dotenv = require('dotenv');
+const connectDB = require('./config/database');
+
+// Import routes
+const authRoutes = require('./routes/authRoutes');
+const tasksRoutes = require('./routes/tasksRoutes');
+const categoriesRoutes = require('./routes/categoriesRoutes');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+// Routes
+app.use("/api/auth",authRoutes);
+app.use("/api/tasks",tasksRoutes);
+app.use("/api/categories",categoriesRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API is running successfully");
+});
+
+// Handle undefined routes (404)
+app.all('*', (req, res, next) => {
+  const error = new Error(`Cannot find ${req.originalUrl} on this server!`);
+  error.statusCode = 404;
+  next(error);
+});
+
+// Global Error Handling Middleware
+app.use((err, req, res, next) => {
+  console.error('ERROR:', err);
+
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    ...(process.env.NODE_ENV === 'development' && { stack: err.stack })
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=>{
+    console.log(`server is running on http://localhost:${PORT}`);
+})
