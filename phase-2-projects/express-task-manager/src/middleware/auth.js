1032afa32ac895cdb3b0401467c4de5665d746c7
VIGNESH <ssvignesh2003@gmail.com>
2025-11-10 22:59:23 +0530

    Files update

---
+++ phase-2-projects/express-task-manager/src/middleware/auth.js
@@ -0,0 +1,38 @@
+const User = require('../models/User');
+const { verifyToken } = require('../utils/jwt');
+
+const protect = async (req, res, next) => {
+  try {
+    let token;
+
+    const authHeader = req.headers.authorization || req.headers.Authorization;
+    if (authHeader && authHeader.startsWith('Bearer ')) {
+      token = authHeader.split(' ')[1];
+    } else if (req.cookies && req.cookies.token) {
+      token = req.cookies.token;
+    }
+
+    if (!token) {
+      return res.status(401).json({ success: false, message: 'Not authorized, token missing' });
+    }
+
+    let decoded;
+    try {
+      decoded = verifyToken(token);
+    } catch (err) {
+      return res.status(401).json({ success: false, message: 'Not authorized, token invalid or expired' });
+    }
+
+    const user = await User.findById(decoded.id).select('-password');
+    if (!user) {
+      return res.status(401).json({ success: false, message: 'Not authorized, user not found' });
+    }
+
+    req.user = user; // attach
+    next();
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { protect };
