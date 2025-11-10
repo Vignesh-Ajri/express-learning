a5578a355de560503884d3964332369f5071cd04
VIGNESH <ssvignesh2003@gmail.com>
2025-11-10 22:47:47 +0530

    Utils Files update

--- phase-2-projects/express-task-manager/src/utils/bcrypt.js
+++ phase-2-projects/express-task-manager/src/utils/bcrypt.js
@@ -1,14 +1,14 @@
 // This is handy for other flows (e.g., password reset)
-// const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 
-// const hashPassword = async (plain) => {
-//   const saltRounds = 10;
-//   const salt = await bcrypt.genSalt(saltRounds);
-//   return bcrypt.hash(plain, salt);
-// };
+const hashPassword = async (plain) => {
+  const saltRounds = 10;
+  const salt = await bcrypt.genSalt(saltRounds);
+  return bcrypt.hash(plain, salt);
+};
 
-// const comparePassword = async (plain, hashed) => {
-//   return bcrypt.compare(plain, hashed);
-// };
+const comparePassword = async (plain, hashed) => {
+  return bcrypt.compare(plain, hashed);
+};
 
-// module.exports = { hashPassword, comparePassword };
+module.exports = { hashPassword, comparePassword };
