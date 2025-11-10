d7baf6859b2e3ab8a5cce9823f645c27736a4166
VIGNESH <ssvignesh2003@gmail.com>
2025-11-10 22:43:18 +0530

    User model update

--- phase-2-projects/express-task-manager/src/models/User.js
+++ phase-2-projects/express-task-manager/src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema(
     {
@@ -10,5 +11,19 @@ const userSchema = mongoose.Schema(
         timestamps: true,
     }
 );
+
+// pre-save hashing: only if password is modified or new
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  try {
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    this.password = await bcrypt.hash(this.password, salt);
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
 
 module.exports = mongoose.model('User', userSchema);
