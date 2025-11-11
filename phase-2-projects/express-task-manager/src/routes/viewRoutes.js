2704c55babf404dec33c5338dccf8a95c1749006
VIGNESH <ssvignesh2003@gmail.com>
2025-11-11 22:44:29 +0530

    Routes update

---
+++ phase-2-projects/express-task-manager/src/routes/viewRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const { protect } = require('../middleware/auth');
+
+// Public Pages
+router.get("/", (req, res) => res.render("index"));
+router.get("/login", (req, res) => res.render("login"));
+router.get("/register", (req, res) => res.render("register"));
+
+// Protected Pages
+router.get("/dashboard", protect, (req, res) => res.render("dashboard"));
+router.get("/tasks", protect, (req, res) => res.render("tasks"));
+router.get("/categories", protect, (req, res) => res.render("categories"));
+
+module.exports = router;
