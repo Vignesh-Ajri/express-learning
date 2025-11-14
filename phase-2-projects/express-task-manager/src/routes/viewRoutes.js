3454b6cbb5603f693db5265459a032cdd34cb47a
VIGNESH <ssvignesh2003@gmail.com>
2025-11-14 23:25:40 +0530

    Routes update

--- phase-2-projects/express-task-manager/src/routes/viewRoutes.js
+++ phase-2-projects/express-task-manager/src/routes/viewRoutes.js
@@ -1,15 +1,14 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { protect } = require('../middleware/auth');
 
-// Public Pages
-router.get("/", (req, res) => res.render("index"));
-router.get("/login", (req, res) => res.render("login"));
-router.get("/register", (req, res) => res.render("register"));
+// Public pages
+router.get('/', (req, res) => res.render('index'));
+router.get('/login', (req, res) => res.render('login'));
+router.get('/register', (req, res) => res.render('register'));
 
-// Protected Pages
-router.get("/me", (req, res) => res.render("dashboard"));
-router.get("/tasks", protect, (req, res) => res.render("tasks"));
-router.get("/categories", protect, (req, res) => res.render("categories"));
+// Protected pages
+router.get('/dashboard', (req, res) => res.render('dashboard'));
+router.get('/tasks', (req, res) => res.render('tasks'));
+router.get('/categories', (req, res) => res.render('categories'));
 
 module.exports = router;
