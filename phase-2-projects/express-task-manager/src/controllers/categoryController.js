4da8efdc997edb7260f46488131f4b02c1939d9c
VIGNESH <ssvignesh2003@gmail.com>
2025-11-14 23:20:23 +0530

    Category Controller update

--- phase-2-projects/express-task-manager/src/controllers/categoryController.js
+++ phase-2-projects/express-task-manager/src/controllers/categoryController.js
@@ -4,21 +4,32 @@ const Category = require('../models/Category');
 const createCategory = async (req, res, next) => {
   try {
     const { name } = req.body;
+
     if (!name) {
-      return res.status(400).json({ success: false, message: 'Category name is required' });
+      return res.status(400).json({ 
+        success: false, 
+        message: 'Category name is required' 
+      });
     }
 
-    const category = await Category.create({ name, createdBy: req.user._id });
+    const category = await Category.create({ 
+      name, 
+      createdBy: req.user._id 
+    });
+
     res.status(201).json({ success: true, data: category });
   } catch (err) {
     next(err);
   }
 };
 
-// Get all categories by user
+// Get all categories
 const getCategories = async (req, res, next) => {
   try {
-    const categories = await Category.find({ createdBy: req.user._id }).sort('-createdAt');
+    const categories = await Category.find({ 
+      createdBy: req.user._id 
+    }).sort('-createdAt');
+
     res.json({ success: true, data: categories });
   } catch (err) {
     next(err);
@@ -29,13 +40,17 @@ const getCategories = async (req, res, next) => {
 const deleteCategory = async (req, res, next) => {
   try {
     const { id } = req.params;
+
     const category = await Category.findOneAndDelete({
       _id: id,
       createdBy: req.user._id
     });
 
     if (!category) {
-      return res.status(404).json({ success: false, message: 'Category not found or unauthorized' });
+      return res.status(404).json({ 
+        success: false, 
+        message: 'Category not found' 
+      });
     }
 
     res.json({ success: true, message: 'Category deleted successfully' });
