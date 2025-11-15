e6659a0b78920f9fd64210c3027931383d9b941d
VIGNESH <ssvignesh2003@gmail.com>
2025-11-15 21:19:57 +0530

    Security Updated

--- phase-2-projects/express-task-manager/src/routes/viewRoutes.js
+++ phase-2-projects/express-task-manager/src/routes/viewRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { protectView, redirectIfAuth } = require('../middleware/viewAuth');
 
 // Public pages
 router.get('/', (req, res) => res.render('index'));
-router.get('/login', (req, res) => res.render('login'));
-router.get('/register', (req, res) => res.render('register'));
+router.get('/login', redirectIfAuth, (req, res) => res.render('login'));
+router.get('/register', redirectIfAuth, (req, res) => res.render('register'));
 
 // Protected pages
-router.get('/dashboard', (req, res) => res.render('dashboard'));
-router.get('/tasks', (req, res) => res.render('tasks'));
-router.get('/categories', (req, res) => res.render('categories'));
+router.get('/dashboard', protectView, (req, res) => res.render('dashboard'));
+router.get('/tasks', protectView, (req, res) => res.render('tasks'));
+router.get('/categories', protectView, (req, res) => res.render('categories'));
 
 module.exports = router;
